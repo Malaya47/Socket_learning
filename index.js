a6442918dee34e8fc8b444912d5bd4d988293ba9
Malaya tiwari <123253350+Malaya47@users.noreply.github.com>
2025-07-03 08:45:27 +0000

    update: basic setup of socket

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  // Each socket also fires a special disconnect event:
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
